c087f192afb39d3d46a5eff3acd5af963e409895
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:55:25 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Commodore 64, 128 and other emulators";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
